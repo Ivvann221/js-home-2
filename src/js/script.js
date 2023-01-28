1b982213cc7726d211c58a2decdc6a64bf139dde
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2023-01-28 09:41:21 +0200

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,72 @@
+
+// 1.
+
+// let age = prompt('Введіть ваш вік');
+// if(age > 0){
+//  if(age <= 12 ){
+//   alert('дитина');
+//  }
+//  if(age > 12 && age <= 18 ){
+//   alert('підліток');
+//  }
+//  if(age > 18 && age < 60 ){
+//   alert('дорослий');
+//  }
+//  if(age >= 60){
+//   alert('пенсіонер');
+//  }
+// }else{
+//   alert('Помилка');
+// }
+
+// 2.
+
+// let number = prompt('Введіть число від 0 до 9');
+// if(number <= 9 && number >= 0){
+//    if(number == 0 ){
+//     alert('~');
+//    }
+//    if(number == 1 ){
+//     alert('!');
+//    }
+//    if(number == 2 ){
+//     alert('@');
+//    }
+//    if(number == 3 ){
+//     alert('#');
+//    }
+//    if(number == 4 ){
+//     alert('$');
+//    }
+//    if(number == 5 ){
+//     alert('%');
+//    }
+//    if(number == 6 ){
+//     alert('^');
+//    }
+//    if(number == 7 ){
+//     alert('&');
+//    }
+//    if(number == 8 ){
+//     alert('*');
+//    }
+//    if(number == 9 ){
+//     alert('(');
+//    }
+//   }else{
+//     alert('Помилка');
+//   }
+
+//3
+
+// let chuslo = prompt('Введіть трьох значне число')
+// if(chuslo >= 100 && chuslo < 1000){
+//   if()
+// }else{
+//   alert('Помилка')
+// }
+
+function name(from, text){
+  alert(from, text);
+}
+name('anna' + 'qqq')
